5c60b3e8396a9e3b2b3828485afd29783f16a363
Amon Bazongo <amonmoce@yahoo.fr>
2023-02-27 20:16:01 +0000

    distance from embeddings

--- app.py
+++ app.py
@@ -21,7 +21,7 @@ def index():
         # Get preprocessed embeddings
         qa = pd.read_csv('./processed_knowledge_base.csv')
         # Get the distances from the embeddings
-        qa['distances'] = qa.embedding.apply(lambda x: distances_from_embeddings(q_embeddings, x, distance_metric='cosine')) 
+        qa['distances'] = distances_from_embeddings(q_embeddings, qa.embedding.tolist(), distance_metric='cosine')
         relevant_text = qa.sort_values('distances', ascending=True)['text'][0]+" "+qa.sort_values('distances', ascending=True)['text'][1]
         response = response = openai.Completion.create(
             prompt=generate_prompt(relevant_text, question),
