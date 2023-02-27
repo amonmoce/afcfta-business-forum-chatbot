e253a29ef1ed33a4e8069fc5918551391664de42
Amon Bazongo <amonmoce@yahoo.fr>
2023-02-27 02:46:03 +0000

    to test now

--- app.py
+++ app.py
@@ -1,35 +1,43 @@
 import os
+import pandas as pd
 
 import openai
+from openai.embeddings_utils import get_embedding, distances_from_embeddings
+
 from flask import Flask, redirect, render_template, request, url_for
 
 app = Flask(__name__)
-openai.api_key = os.getenv("OPENAI_API_KEY")
+app.debug = True
 
+openai.api_key = os.getenv("OPENAI_API_KEY")
+embedding_model = "text-embedding-ada-002"
+gpt_model = "text-davinci-003"
 
 @app.route("/", methods=("GET", "POST"))
 def index():
     if request.method == "POST":
-        animal = request.form["animal"]
-        response = openai.Completion.create(
-            model="text-davinci-003",
-            prompt=generate_prompt(animal),
-            temperature=0.6,
+        question = request.form["question"]
+        q_embeddings = get_embedding(question, engine=embedding_model)
+        # Get preprocessed embeddings
+        qa = pd.read_csv('./processed_knowledge_base.csv')
+        # Get the distances from the embeddings
+        qa['distances'] = distances_from_embeddings(q_embeddings, qa['embedding'].values, distance_metric='cosine')
+        relevant_text = qa.sort_values('distances', ascending=True)['text'][0]
+        response = response = openai.Completion.create(
+            prompt=generate_prompt(relevant_text, question),
+            temperature=0,
+            max_tokens=128,
+            # top_p=1,
+            # frequency_penalty=0,
+            # presence_penalty=0,
+            stop=["###", "\n\n"],
+            model=gpt_model
         )
-        return redirect(url_for("index", result=response.choices[0].text))
+        return response.choices[0].text.strip()
 
     result = request.args.get("result")
     return render_template("index.html", result=result)
 
 
-def generate_prompt(animal):
-    return """Suggest three names for an animal that is a superhero.
-
-Animal: Cat
-Names: Captain Sharpclaw, Agent Fluffball, The Incredible Feline
-Animal: Dog
-Names: Ruff the Protector, Wonder Canine, Sir Barks-a-Lot
-Animal: {}
-Names:""".format(
-        animal.capitalize()
-    )
+def generate_prompt(relevant_text, question):
+    return f"Answer the question based on the context below, and if the question can't be answered based on the context, say \"Please contact the AfCFTA for this particular question\"\n\nContext: {relevant_text}\n\n---\n\nQuestion: {question}\nAnswer:"
