77f8e0ac3de55a0bf0a71ee46bacf0b094ff1574
Amon Bazongo <amonmoce@yahoo.fr>
2023-05-22 10:59:12 +0000

    for new users

--- app.py
+++ app.py
@@ -113,12 +113,14 @@ def webhook():
                     msg_body = messages[0]['text']['body'] # extract the message text from the webhook payload
                     # Get info from database about the chatbot mode
                     chatpawa_users = supabase.table("chatpawa-users").select("*").eq("phone_number", from_number).execute()
-                    phone_number_mode = chatpawa_users.data[0]['phone_number_mode']
                     # phone_number_lang = chatpawa_users.data[0]['phone_number_lang']
-                    if phone_number_mode == "":
+                    phone_number_mode = ""
+                    if len(chatpawa_users.data) == 0:
                         default_phone_number_mode = supabase.table("modes-settings").select("*").eq("status", "default").execute()
                         phone_number_mode = default_phone_number_mode.data[0]['mode_id']
                         data = supabase.table("chatpawa-users").insert({"phone_number":from_number, "phone_number_mode": phone_number_mode}).execute()
+                    else:
+                        phone_number_mode = chatpawa_users.data[0]['phone_number_mode']
                     modes_setting = supabase.table("modes-settings").select("*").eq("mode_id", phone_number_mode).execute()
                     system_message = modes_setting.data[0]['mode_system_message']
                     print(msg_body)
